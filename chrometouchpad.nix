05bb316ea1ba7a355383a007a08960fe9d5f4140
Calvin Kim <ccychc@gmail.com>
2019-06-19 07:44:34 +0000

    Update chrometouchpad.nix

--- chrometouchpad.nix
+++ chrometouchpad.nix
@@ -1,10 +1,11 @@
-{ stdenv, };
+{ stdenv }:
 
 stdenv.mkDerivation {
   name = "xf86-input-cmt";
   src = fetchFromGithub {
     owner = "hugegreenbug";
     repo = "xf86-input-cmt";
+    sha256 = "1vwndgidlvirpjx7841a7i8fvxybbh8xjxdsgq2azqi3gjrggvay"
   }
   phases = "installPhase";
 

18c602eb8a7b597f9dbe151fb41c87622fd5b273
Calvin Kim <ccychc@gmail.com>
2019-06-19 16:42:26 +0900

    additional stuff

---
+++ chrometouchpad.nix
@@ -0,0 +1,15 @@
+{ stdenv, };
+
+stdenv.mkDerivation {
+  name = "xf86-input-cmt";
+  src = fetchFromGithub {
+    owner = "hugegreenbug";
+    repo = "xf86-input-cmt";
+  }
+  phases = "installPhase";
+
+  # bash script for how my derivation is built
+  installPhase = ''
+    
+  '';
+}
--- configuration.nix
+++ configuration.nix
@@ -9,48 +9,64 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
-    networking.hostname = "bitcoin";
-    networking.wireless.enable = true;
-    #allow shit programs
-    nixpkgs.config.allowUnfree = true;
+  networking.hostname = "bitcoin";
+  networking.wireless.enable = true;
+  #allow shit programs
+  nixpkgs.config.allowUnfree = true;
     
-    environment.systemPackages = [
-      pkgs.firefox
-      pkgs.mosh
-      pkgs.git
-      pkgs.tmux
-      pkgs.tree
-      pkgs.python
-      pkgs.gcc
-      pkgs.vim
-    ];
+  environment.systemPackages = with pkgs; [
+    firefox
+    mosh
+    git
+    tmux
+    tree
+    python
+    gcc
+    vim
+    vlc
+    gnumake
+    unzip
+    blueman
+    gnupg
+    jsoncpp
+	#needed for the window manager
+	rofi
+    xorg.xbacklight
+    lm_sensors
+    jq
+    maim
+    i3lock
+  ];
     
-    i18n = {
-      consoleFont = "Lat2-Terminus16";
-      consoleKeyMap = "us";
-      defaultLocale = "en_US.UTF-8";
-    };
+  #Locale
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
     
-    i18n.inputMethod = {
-      enabled = "uim";
-    };
+  i18n.inputMethod = {
+    enabled = "uim";
+  };
+  
+  #timezone
+  time.timeZone = "Asia/Seoul";
     
-    time.timeZone = "Asia/Seoul";
+  #audio
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
     
-    sound.enable = true;
-    hardware.pulseaudio.enable = true;
+  services.xserver.enable = true;
+  services.xserver.desktopManager.windowManager.awesome = {
+    enable = true;
+  }
     
-    services.xserver.enable = true;
-    services.xserver.desktopManager = {
-      xfce.enable = true;
-      default = "xfce";
-    }
+  #TODO add chromeos driver
+  services.xserver.libinput.enable = true;
     
-    #TODO add chromeos driver
-    services.xserver.libinput.enable = true;
-    
-    users.users.username = { #choose a username
-      isNormalUser = true;
-      extraGroups = [ "wheel, networkmanager" ];
-    };
-    system.stateVersion = "19.03";
+  users.users.calvin = { #choose a username
+    isNormalUser = true;
+    home = "/home/calvin";
+    extraGroups = [ "wheel, networkmanager" ];
+  };
+  system.stateVersion = "19.03";

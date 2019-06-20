01058b37ef9538f05e4bb9bb69879d304bd73fa0
Calvin Kim <ccychc@gmail.com>
2019-06-21 00:53:21 +0900

    stuff

--- chrometouchpad.nix
+++ chrometouchpad.nix
@@ -1,17 +1,20 @@
-{ stdenv }:
+{ stdenv, fetchFromGithub }:
 
 stdenv.mkDerivation {
   name = "xf86-input-cmt";
+  version = "0.01";
+
   src = fetchFromGithub {
     owner = "hugegreenbug";
     repo = "xf86-input-cmt";
-<<<<<<< HEAD
-    }
-=======
-    sha256 = "1vwndgidlvirpjx7841a7i8fvxybbh8xjxdsgq2azqi3gjrggvay"
->>>>>>> 05bb316ea1ba7a355383a007a08960fe9d5f4140
+    sha256 = "1vwndgidlvirpjx7841a7i8fvxybbh8xjxdsgq2azqi3gjrggvay";
   }
   phases = "installPhase";
+  
+  buildPhase = ''
+
+
+  '';
 
   # bash script for how my derivation is built
   installPhase = ''

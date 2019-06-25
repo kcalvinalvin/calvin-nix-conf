1db983d0afd97f2e4ef8aaa1fc07c1218371a379
Calvin Kim <ccychc@gmail.com>
2019-06-26 02:00:20 +0900

    major changes

--- configuration.nix
+++ configuration.nix
@@ -51,7 +51,7 @@
   hardware.pulseaudio.enable = true;
     
   services.xserver.enable = true;
-  services.xserver.desktopManager.plasma5 = {
+  services.xserver.desktopManager.xfce = {
     enable = true;
   };
     
